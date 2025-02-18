d15a2f1ea1ae9a894f220b8aa2dacce887e8851c
hu-tree <huarthur2@gmail.com>
2025-02-18 16:12:22 -0500

    Made simple_publisher node

--- ros_exercises/setup.py
+++ ros_exercises/setup.py
@@ -4,22 +4,23 @@ package_name = 'ros_exercises'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='racecar',
-    maintainer_email='huarthur2@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="racecar",
+    maintainer_email="huarthur2@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "talker = ros_exercises.simple_publisher:main",
+            # "listener = ros_exercises.simple_subscriber:main",
         ],
     },
 )
