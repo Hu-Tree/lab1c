d6f6a00261ebabae81697c1db474942881138d3a
hu-tree <huarthur2@gmail.com>
2025-02-19 06:43:07 -0500

    Made open_space_publisher and related files

---
+++ ros_exercises/ros_exercises/open_space_publisher.py
@@ -0,0 +1,55 @@
+import rclpy
+import random
+import math
+from rclpy.node import Node
+
+from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
+
+
+class OpenSpacePublisher(Node):
+
+    def __init__(self):
+        super().__init__("open_space_publisher")
+        self.distance_publisher_ = self.create_publisher(
+            Float32, "open_space/distance", 10
+        )
+        self.angle_publisher_ = self.create_publisher(Float32, "open_space/angle", 10)
+        self.subscription = self.create_subscription(
+            LaserScan, "fake_scan", self.listener_callback, 10
+        )
+
+    def listener_callback(self, scan):
+        maxDist = scan.range_min
+        maxIndex = -1
+        for index, dist in enumerate(scan.ranges):
+            if dist > maxDist:
+                maxIndex, maxDist = index, dist
+        distMsg = Float32()
+        distMsg.data = maxDist
+        self.distance_publisher_.publish(distMsg)
+
+        angleMsg = Float32()
+        angleMsg.data = maxIndex * scan.angle_increment + scan.angle_min
+        self.angle_publisher_.publish(angleMsg)
+
+        self.get_logger().info("Max Distance: %s" % distMsg.data)
+        self.get_logger().info("Angle: %s" % angleMsg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    open_space_publisher = OpenSpacePublisher()
+
+    rclpy.spin(open_space_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    open_space_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
